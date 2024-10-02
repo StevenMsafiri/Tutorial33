79c682c4ec403a6d16e09dbd9bbeaff8ef6db737
StevenMsafiri <ianleonardmk47@gmail.com>
2024-10-02 16:08:30 +0300

    Add files via upload
    setuo

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import requests
+LATITUDE = -6.792354
+LONGITUDE = 39.208328
 
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # if response.status_code == 404:
+# #     raise Exception("Not Found, the resource does ot exist.")
+# # elif response.status_code != 200:
+# #     raise Exception("You are not allowed to access this resource.")
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# if response.status_code == 404:
-#     raise Exception("Not Found, the resource does ot exist.")
-# elif response.status_code != 200:
-#     raise Exception("You are not allowed to access this resource.")
+parameter = {
+    "lat": LATITUDE,
+    "lng": LONGITUDE,
+}
 
+response = requests.get(url=f"https://api.sunrise-sunset.org/json", params=parameter)
 data = response.json()
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-
-iss_position = (longitude, latitude)
-print(iss_position)
+print(data)
